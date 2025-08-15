5cb578fd7b1068ebc5d5273186523193a59e678a
fanteng.kong <fanteng.kong@ctechm.com>
2025-08-15 19:10:29 +0400

    feat: 配置用户站点部署，支持根域名访问

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Trash Can",
   description: "Code records at work",
+  base: "/", // 设置为根路径，支持用户站点部署
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
